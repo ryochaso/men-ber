63692040c3c3945f6c4162b13be66d473892254b
ryosuke <64516382+ryochaso@users.noreply.github.com>
2020-12-14 19:10:39 +0900

    ログイン機能の実装

---
+++ app/controllers/photos_controller.rb
@@ -0,0 +1,12 @@
+class PhotosController < ApplicationController
+
+  def index
+    
+  end
+
+  # def new
+  #   @photo = Photo.new
+  # end
+  
+
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,18 @@
 class User < ApplicationRecord
+
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  with_options presence: true do
+    validates :nickname,uniqueness: true
+    validates :email, uniqueness: true
+    validates :password, format: {with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i }
+    validates :first_name, format: {with: /\A[a-zA-Z]+\z/}
+    validates :family_name, format: {with:  /\A[a-zA-Z]+\z/}
+    validates :country, format: {with: /\A[a-zA-Z]+\z/}
+    
+  end
 end
